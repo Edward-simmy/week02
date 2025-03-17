5336e1951b30791a8e6989faddd6e55dee417dd2
Edward Simmy <simmyedward4@gmail.com>
2025-03-18 01:20:34 +0300

    pages done

---
+++ temples.js
@@ -0,0 +1,9 @@
+document.getElementById("placeholder").innerHTML=new Date(document.lastModified);
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
